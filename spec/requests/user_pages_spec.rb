53e458c5c2ea6766a2ab2d0ea5590220881b3c6e
Jesus Torres <jtorres0876@gmail.com>
2013-03-15 12:40:46 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,7 +13,7 @@ describe "User pages" do
 
   describe "profile page" do
   	let (:user) { FactoryGirl.create(:user)}
-  	before { visit user_path(user) }
+  	before { visit user_path(user, size: 40) }
 
   	it { should have_selector('h1', text: user.name) }
   	it { should have_selector('title', text: user.name) }
@@ -29,6 +29,19 @@ describe "User pages" do
   		it "should not create a user" do
   			expect { click_button submit }.not_to change(User, :count)
   		end
+
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should have_content('Name can\'t be blank') }
+        it { should have_content('Email can\'t be blank') }
+        it { should have_content('Email is invalid') }
+        it { should have_content('Password can\'t be blank') }
+        it { should have_content('Password is too short') }
+        it { should have_content('Password confirmation can\'t be blank') }
+      end  
   	end
 
   	describe "with valid information" do
@@ -37,11 +50,20 @@ describe "User pages" do
         	fill_in "Email",        with: "user@example.com"
         	fill_in "Password",     with: "foobar"
         	fill_in "Confirmation", with: "foobar"
-      	end
+      end
 
-      	it "should create a user" do
+      it "should create a user" do
       		expect { click_button submit }.to change(User, :count).by(1)
-      	end
+      end
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
+
+          it { should have_selector('title', text: user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+          it { should have_link('Sign out') }
+        end
     end
    end
 end
